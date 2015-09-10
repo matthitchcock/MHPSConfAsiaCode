9e28d8b4bca03172a5c993cd396ae3b969677f54
Matt Hitchcock <matthitchcock@outlook.com>
2015-09-10 16:52:22 +0800

    First Commit of Invoke-HulkSmash
    This is the first working version of the code.

---
+++ Invoke-HulkSmash.ps1
@@ -0,0 +1,35 @@
+﻿function Invoke-HulkSmash
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $WhatToSmash,
+
+        # Param2 help description
+        [int]
+        $NumberOfSmashes
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $x = 1
+        do
+        {
+            Write-Host -ForegroundColor Green "Hulk is currently smashing $WhatToSmash, $x of $NumberOfSmashes"
+            Sleep -Seconds 2
+            $x ++
+        }
+        until ($x -gt $NumberOfSmashes)
+
+    }
+    End
+    {
+    }
+}
